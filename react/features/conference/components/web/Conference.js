2bbf73b2d9bdae1eb94279fda278a27d5047de34
Lauren McCarthy <laurenleemccarthy@gmail.com>
2021-01-27 12:38:09 -0800

    tweak timing

--- react/features/conference/components/web/Conference.js
+++ react/features/conference/components/web/Conference.js
@@ -520,7 +520,8 @@ class Conference extends AbstractConference<Props, *> {
     _playPrompt = (msg, doSpeak) => {
         $('#notif').text(msg);
         this._groupChatMessage({msg: msg, userName: 'Serenity'});
-        $('#notif-holder').stop().fadeIn(300).delay(msg.length*75).fadeOut(300);
+        let msgDur = Math.max(msg.length*75, 1000);
+        $('#notif-holder').stop().fadeIn(300).delay(msgDur).fadeOut(300);
         if (doSpeak) this._speak(msg);
         console.log('LM _playPrompt: ' + msg);
     }
